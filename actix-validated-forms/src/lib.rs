698c9b45e6d351879e945e2a5c1514da751d5b69
Jacob Halsey <jacob@jhalsey.com>
2020-09-01 19:23:13 +0100

    reexport dependencies

--- actix-validated-forms/src/lib.rs
+++ actix-validated-forms/src/lib.rs
@@ -1,8 +1,11 @@
 #[cfg(test)]
 #[macro_use]
-extern crate validator_derive;
+pub extern crate validator_derive;
 
 pub mod error;
 pub mod form;
 pub mod multipart;
 pub mod query;
+
+pub use validator;
+pub use tempfile;

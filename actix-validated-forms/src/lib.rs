030d6a43f497ebe887ca62fb2f3a56d9a3e1520e
Jacob Halsey <jacob@jhalsey.com>
2020-09-06 18:09:05 +0100

    reexport derive

--- actix-validated-forms/src/lib.rs
+++ actix-validated-forms/src/lib.rs
@@ -9,3 +9,13 @@ pub mod query;
 
 pub use tempfile;
 pub use validator;
+
+
+// Re-export derive
+#[cfg(feature = "derive")]
+#[allow(unused_imports)]
+#[macro_use]
+extern crate actix_validated_forms_derive;
+#[cfg(feature = "derive")]
+#[doc(hidden)]
+pub use actix_validated_forms_derive::*;
